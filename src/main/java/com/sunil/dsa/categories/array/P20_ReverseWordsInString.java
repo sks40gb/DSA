ae7bcf2a33309316bc92d5bd2a635b5eae290a70
Sunil Singh <sks.256@gmail.com>
2025-06-21 22:58:47 +0530

    Reverse Words in a String

--- src/main/java/com/sunil/dsa/categories/array/P20_ReverseWordsInString.java
+++ src/main/java/com/sunil/dsa/categories/array/P20_ReverseWordsInString.java
@@ -1,5 +1,7 @@
 package com.sunil.dsa.categories.array;
 
+import java.util.ArrayList;
+
 /**
  * Example 1:
  *
@@ -11,20 +13,35 @@ public class P20_ReverseWordsInString {
     public static void main(String[] args) {
         P20_ReverseWordsInString o = new P20_ReverseWordsInString();
         String s = "the sky is blue";
-        System.out.println(o.reverseWords(s));
+        System.out.println(o.reverseWords1(s));
+        System.out.println(o.reverseWords2(s));
     }
 
-    public String reverseWords(String s) {
+    public String reverseWords1(String s) {
         String[] words = s.split(" ");
         String result = "";
         for (int i = words.length - 1; i >= 0; i--) {
-            if(result == ""){
+            if (result == "") {
                 result = words[i];
-            }else{
+            } else {
                 result += " " + words[i];
             }
         }
         return result;
+    }
+
+    public String reverseWords2(String s) {
+        String[] words = s.split("\\s+");
+        int left = 0;
+        int right = words.length - 1;
+        while (left < right) {
+            String temp = words[left];
+            words[left] = words[right];
+            words[right] = temp;
+            left++;
+            right--;
+        }
+        return String.join(" ", words);
     }
 
 }
