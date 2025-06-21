60da9b140803124b5e44a8bd4142d4ae7ab721fd
Sunil Singh <sks.256@gmail.com>
2025-06-21 14:03:30 +0530

    Added test class

---
+++ src/test/java/array/P13_ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,28 @@
+package array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class P13_ProductOfArrayExceptSelfTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void productExceptSelf() {
+    }
+
+    @Test
+    void solution1() {
+    }
+
+    @Test
+    void solution2() {
+    }
+
+    @Test
+    void solution3() {
+    }
+}
