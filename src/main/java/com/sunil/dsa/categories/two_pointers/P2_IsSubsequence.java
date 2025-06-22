85983c03fa461ee98eef44efee72409010ba08a9
Sunil Singh <sks.256@gmail.com>
2025-06-22 14:48:17 +0530

    Is Subsequence

---
+++ src/main/java/com/sunil/dsa/categories/two_pointers/P2_IsSubsequence.java
@@ -0,0 +1,46 @@
+package com.sunil.dsa.categories.two_pointers;
+
+/*
+https://leetcode.com/problems/is-subsequence/description/
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+ */
+public class P2_IsSubsequence {
+
+    public static void main(String[] args) {
+        P2_IsSubsequence o = new P2_IsSubsequence();
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(o.isSubsequence(s, t));
+    }
+
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) {
+            return true;
+        }
+        if (t.length() == 0) {
+            return false;
+        }
+
+        int sIndex = 0;
+        int tIndex = 0;
+        while (sIndex < s.length() && tIndex < t.length()) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                sIndex++;
+                tIndex++;
+            } else {
+                tIndex++;
+            }
+        }
+        return sIndex == s.length();
+    }
+
+}
