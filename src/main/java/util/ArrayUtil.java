d18c301c84a332fa8bda004281f7ad5642114ed7
Sunil Singh <sks.256@gmail.com>
2025-06-24 20:46:09 +0530

    Minimum Window Substring

--- src/main/java/util/ArrayUtil.java
+++ src/main/java/util/ArrayUtil.java
@@ -2,7 +2,7 @@ package util;
 
 public class ArrayUtil {
 
-    public static void print2D(Integer[][] matrix){
+    public static void print2D(Integer[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(" " + matrix[i][j]);
@@ -11,7 +11,7 @@ public class ArrayUtil {
         }
     }
 
-    public static void print2D(int[][] matrix){
+    public static void print2D(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(" " + matrix[i][j]);
@@ -46,5 +46,13 @@ public class ArrayUtil {
             System.out.print(" " + arr[i]);
         }
         System.out.println();
+    }
+
+    public static void printFrequency(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                System.out.println((char) i + " : " + arr[i]);
+            }
+        }
     }
 }
