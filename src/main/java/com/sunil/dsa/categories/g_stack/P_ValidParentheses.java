d1c49d77a10a2a1cb18589663fea70f3418432de
Sunil Singh <sks.256@gmail.com>
2025-07-02 23:22:45 +0530

    Valid Parentheses

---
+++ src/main/java/com/sunil/dsa/categories/g_stack/P_ValidParentheses.java
@@ -0,0 +1,48 @@
+package com.sunil.dsa.categories.g_stack;
+
+import java.util.Stack;
+
+/*
+https://leetcode.com/problems/valid-parentheses/
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+ */
+
+public class P_ValidParentheses {
+
+    public static void main(String[] args) {
+        P_ValidParentheses o = new P_ValidParentheses();
+        String s = "()[]{}";
+        System.out.println(o.isValid(s));
+    }
+
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char fromChar = 'x';
+            if(!stack.isEmpty()){
+                fromChar = stack.peek();
+            }
+            char toChar = s.charAt(i);
+            if ((toChar == '}' && fromChar == '{') || (toChar == ')' && fromChar == '(') || (toChar == ']' && fromChar == '[')) {
+                stack.pop();
+            }else{
+                stack.push(toChar);
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
