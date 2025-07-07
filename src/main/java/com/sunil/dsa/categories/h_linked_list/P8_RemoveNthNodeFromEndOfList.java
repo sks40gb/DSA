0c04dc63def3ba63a1daf2479b9b027d766fe24b
Sunil Singh <sks.256@gmail.com>
2025-07-07 20:06:05 +0530

    Remove Nth Node From End of List

--- src/main/java/com/sunil/dsa/categories/h_linked_list/P8_RemoveNthNodeFromEndOfList.java
+++ src/main/java/com/sunil/dsa/categories/h_linked_list/P8_RemoveNthNodeFromEndOfList.java
@@ -4,6 +4,8 @@ import t150.linkedlist.ListNode;
 import util.LinkedListUtil;
 
 /*
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
 Example 1:
 Input: head = [1,2,3,4,5], n = 2
 Output: [1,2,3,5]
